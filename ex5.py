86dee6bd97c2edef5f9abaa04797b2d69c616c06
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-10-29 09:08:35 +0530

    Exercise 13: Study about Parameters,Unpacking,Variables

--- ex5.py
+++ ex5.py
@@ -15,6 +15,5 @@ print(f"Actually that's too heavy.")
 print(f"He's got {eyes} eyes and and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")
 
-# this line is tricky, try to get it exactly right
 total = my_age + height + weight
 print(f"If I add {my_age}, {height}, and {weight} I get {total}.")
