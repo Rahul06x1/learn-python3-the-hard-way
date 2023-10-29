00b589ecf8967a03ef0d2f8cc3ef0d983b47b8fc
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-10-29 12:31:31 +0530

    Exercise 21: Functions Can Return Something

---
+++ ex21.py
@@ -0,0 +1,28 @@
+def add(a,b):
+    print(f"Adding {a} & {b}")
+    return a + b
+
+def subtract(a,b):
+    print(f"Subtrscting {a} & {b}")
+    return a - b
+
+def multiply(a,b):
+    print(f"Multiplying {a} & {b}")
+    return a * b
+
+def divide(a,b):
+    print(f"Dividing {a} & {b}")
+    return a / b
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what)
